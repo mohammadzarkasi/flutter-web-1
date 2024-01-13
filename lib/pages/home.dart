609c6c67a277f8a4a709d845a5655c5446e96452
mohammadzarkasi <mohammad.zarkasi@gmail.com>
2024-01-13 21:03:37 +0700

    appbar untuk desktop dan mobile

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -29,20 +29,73 @@ class HomePage2 extends StatelessWidget {
       desktop: _buildDesktop,
       tablet: _buildTablet,
     );
+
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     title: Text('Selamat Datang'),
+    //   ),
+    // );
   }
 
   Widget _buildMobile(BuildContext ctx) {
     print('build mobile');
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        // title: Text(
+        //   'FWEB-1',
+        //   style: Theme.of(ctx).primaryTextTheme.titleLarge,
+        // ),
+        title: _buildLogo(ctx),
+        backgroundColor: Theme.of(ctx).primaryColor,
+        foregroundColor: Theme.of(ctx).cardColor,
+        leading: IconButton(icon: Icon(Icons.menu), onPressed: () {}),
+      ),
+    );
   }
 
   Widget _buildDesktop(BuildContext ctx) {
     print('build desktop');
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        // toolbarHeight: 200,
+        title: Container(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              _buildLogo(ctx),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text('Menu 1'),
+                  Text('Menu 2'),
+                  Text('Menu 3'),
+                  Text('Menu 4'),
+                ],
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('Request'),
+              ),
+            ],
+          ),
+        ),
+        // title: _buildLogo(ctx),
+        backgroundColor: Theme.of(ctx).primaryColor,
+        foregroundColor: Theme.of(ctx).cardColor,
+      ),
+    );
   }
 
   Widget _buildTablet(BuildContext ctx) {
     print('build tablet');
     return Scaffold();
+  }
+
+  Widget _buildLogo(BuildContext ctx) {
+    return Text(
+      'FWEB-1',
+      style: Theme.of(ctx).primaryTextTheme.titleLarge,
+    );
   }
 }
